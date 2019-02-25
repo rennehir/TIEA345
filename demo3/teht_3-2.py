3dd487989db09a3fd7acc2c5940d6395efb173a0
rennehir <renne@rennehir.fi>
2019-02-25 16:14:29 +0200

    Write to sheets

--- demo3/teht_3-2.py
+++ demo3/teht_3-2.py
@@ -1,3 +1,4 @@
+import sys
 import Adafruit_DHT
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -8,11 +9,5 @@ scope = ['https://spreadsheets.google.com/feeds',
 credentials = ServiceAccountCredentials.from_json_keyfile_name('../../../credentials.json', scope)
 
 gc = gspread.authorize(credentials)
-
 sht1 = gc.open("hirsimaki_raspberry-temp-hum").sheet1
-
-while True:
-  humidity, temperature = Adafruit_DHT.read_retry(11, 21)
-  print 'Temperature: {0:0.1f} C    Humidity: {1:0.1f} %'.format(temperature, humidity)
-  sht1.append_row([humidity, temperature])
-  time.sleep(3)
+sht1.append_row(["eka", "toka"])
