52eeacaf0bab038393bbda2d87ce667befc4f8b4
rennehir <git@rennehir.fi>
2019-02-25 10:37:23 +0200

    Add demo2 teht2 boilerplate

---
+++ demo2/teht_2-2.py
@@ -0,0 +1,16 @@
+import time
+import RPi.GPIO as GPIO
+
+led = 21
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+
+for x in range(5):
+  GPIO.output(led, GPIO.HIGH)
+  time.sleep(1)
+
+  GPIO.output(led, GPIO.LOW)
+  time.sleep(1)
+
+GPIO.cleanup()
