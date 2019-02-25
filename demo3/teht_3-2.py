dc63f75377cd045b5bd20ffc737805b426fb05cb
rennehir <git@rennehir.fi>
2019-02-25 16:16:29 +0200

    Print data

--- demo3/teht_3-2.py
+++ demo3/teht_3-2.py
@@ -1,7 +1,17 @@
 import sys
-import Adafruit_DHT
+import Adafruit_DHT.Adafruit_DHT as Adafruit_DHT
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+
+sensor = Adafruit_DHT.DHT11
+pin = 21
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+if humidity is not None and temperature is not None:
+  print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+else:
+  print 'Failed to get reading. Try again!'
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
