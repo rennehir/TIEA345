3ed54ba9046cdac87897cf95a1b74444b982b348
rennehir <git@rennehir.fi>
2019-02-25 12:09:38 +0200

    Add car lights

--- demo2/teht_2-3.py
+++ demo2/teht_2-3.py
@@ -6,22 +6,21 @@ GPIO.setmode(GPIO.BCM)
 button = 19
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-led = 21
-GPIO.setup(led, GPIO.OUT)
+car_red = 16
+GPIO.setup(car_red, GPIO.OUT)
 
-pir = 18
-GPIO.setup(pir, GPIO.IN)
+car_yellow = 20
+GPIO.setup(car_yellow, GPIO.OUT)
+
+car_green = 21
+GPIO.setup(car_green, GPIO.OUT)
 
 while True:
   button_state = GPIO.input(button)
 
-  if button_state == False:
-    pir_state = GPIO.input(pir)
-    print('Button pressed')
-    print('PIR state:', pir_state)
-    time.sleep(0.2)
-    GPIO.output(led, 1)
-  else :
-    GPIO.output(led, 0)
+  if button_state = True:
+    GPIO.output(car_red, 1)
+    GPIO.output(car_yellow, 1)
+    GPIO.output(car_green, 1)
 
 GPIO.cleanup()
