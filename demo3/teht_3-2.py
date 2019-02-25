e72be94f0a4c15c6ffc427efcff06bae42219297
rennehir <renne@rennehir.fi>
2019-02-25 16:25:59 +0200

    Fix print

--- demo3/teht_3-2.py
+++ demo3/teht_3-2.py
@@ -1,5 +1,5 @@
 import sys
-import Adafruit_DHT.Adafruit_DHT as Adafruit_DHT
+import Adafruit_DHT
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -9,9 +9,9 @@ pin = 21
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
-  print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+  print("Temp: %0.1f C  Humidity: %0.1f" % (temperature, humidity))
 else:
-  print 'Failed to get reading. Try again!'
+  print("Failed to get reading. Try again!")
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
