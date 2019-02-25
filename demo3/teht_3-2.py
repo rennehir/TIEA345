a0c1174f484f7d411268353f3340c7178354d8a1
rennehir <renne@rennehir.fi>
2019-02-25 15:53:20 +0200

    teht_3-2

--- demo3/teht_3-2.py
+++ demo3/teht_3-2.py
@@ -4,8 +4,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('../../credentials.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('../../../credentials.json', scope)
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open("Where is the money Lebowski?").sheet1
+wks = gc.open("hirsimaki_raspberry-temp-hum").sheet1
