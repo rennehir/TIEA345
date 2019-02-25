89542b676a1f0e8e49ba3d40147bd29ccfaeba28
rennehir <git@rennehir.fi>
2019-02-25 10:44:23 +0200

    Add button

--- demo2/teht_2-2.py
+++ demo2/teht_2-2.py
@@ -1,16 +1,21 @@
 import time
 import RPi.GPIO as GPIO
 
-led = 21
-
 GPIO.setmode(GPIO.BCM)
+
+button = 19
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PDU_UP)
+
+led = 21
 GPIO.setup(led, GPIO.OUT)
 
-for x in range(5):
-  GPIO.output(led, GPIO.HIGH)
-  time.sleep(1)
-
-  GPIO.output(led, GPIO.LOW)
-  time.sleep(1)
+while True:
+  input_state = GPIO.input(button)
+  if input_state == False:
+    print('Button pressed')
+    time.sleep(0.2)
+    GPIO.output(led, 1)
+  else :
+    GPIO.output(led, 0)
 
 GPIO.cleanup()
