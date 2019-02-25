49df58567cd8b34215ceb59a4f5723b06c486133
rennehir <git@rennehir.fi>
2019-02-25 17:07:09 +0200

    Take video

--- demo3/teht_3-3.py
+++ demo3/teht_3-3.py
@@ -4,6 +4,16 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-sleep(5)
+sleep(2)
 camera.capture('./images/image.jpg')
+camera.stop_preview()
+
+sleep(2)
+
+camera.resolution = (720, 540)
+camera.start_preview()
+sleep(2)
+camera.start_recording('/home/pi/video.h264')
+sleep(15)
+camera.stop_recording()
 camera.stop_preview()
