48354550cf2a1ef3cd4903adf8b58fb2ea3b3578
rennehir <renne@rennehir.fi>
2019-02-25 11:31:00 +0200

    Print nicer

--- demo2/teht_2-2.py
+++ demo2/teht_2-2.py
@@ -15,11 +15,11 @@ GPIO.setup(pir, GPIO.IN)
 
 while True:
   button_state = GPIO.input(button)
-  pir_state = GPIO.input(pir)
 
   if button_state == False:
+    pir_state = GPIO.input(pir)
     print('Button pressed')
-    print(pir_state)
+    print('PIR state:', pir_state)
     time.sleep(0.2)
     GPIO.output(led, 1)
   else :
