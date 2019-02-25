e0a00f6e14b30957bb3ddbb0a8c6db6844d0922b
rennehir <git@rennehir.fi>
2019-02-25 16:31:37 +0200

    Add timestamp and data every 5 seconds

--- demo3/teht_3-2.py
+++ demo3/teht_3-2.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import datetime
 import Adafruit_DHT
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -13,10 +15,13 @@ sht1 = gc.open("hirsimaki_raspberry-temp-hum").sheet1
 sensor = Adafruit_DHT.DHT11
 pin = 21
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while True:
+  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-if humidity is not None and temperature is not None:
-  sht1.append_row([temperature, humidity])
-  print("Temp: %0.1f C  Humidity: %0.1f" % (temperature, humidity))
-else:
-  print("Failed to get reading. Try again!")
+  if humidity is not None and temperature is not None:
+    now = datetime.datetime.now()
+    sht1.append_row([now, temperature, humidity])
+  else:
+    print("Failed to get reading. Try again!")
+
+  time.sleep(5)
