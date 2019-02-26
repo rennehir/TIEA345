cfa3a596e5fab51d343fed67b8389e45f35e73af
rennehir <git@rennehir.fi>
2019-02-26 13:49:35 +0200

    Send file

--- demo3/teht_3-6/index.js
+++ demo3/teht_3-6/index.js
@@ -6,26 +6,30 @@ const imagePath = '/home/pi/code/TIEA345/demo3/cronimages'
 const app = express()
 
 const getNewestFile = (files, path) => {
-    const out = [];
+    const out = []
+
     files.forEach(file => {
-        const stats = fs.statSync(path + '/' + file);
+        const stats = fs.statSync(path + '/' + file)
         if(stats.isFile()) {
-            out.push({'file':file, 'mtime': stats.mtime.getTime()});
+            out.push({'file':file, 'mtime': stats.mtime.getTime()})
         }
-    });
-    out.sort((a,b) => {
-        return b.mtime - a.mtime;
     })
-    return (out.length>0) ? out[0].file : '';
+
+    out.sort((a,b) => {
+        return b.mtime - a.mtime
+    })
+
+    return (out.length>0) ? out[0].file : ''
 }
 
 app.get('/latestImage', (req, res) => {
     fs.readdir(imagePath, function(err, files) {
-        if (err) { throw err; }
-        var audioFile = getNewestFile(files, imagePath).replace('.wav', '.mp3');
-        //process audioFile here or pass it to a function...
-        console.log(audioFile);
-    });
+        if (err) { throw err }
+
+        const latestImage = getNewestFile(files, imagePath)
+
+        res.sendFile(latestImage)
+    })
 })
 
 app.listen(3000, () => {
